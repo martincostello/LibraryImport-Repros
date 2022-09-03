1ce43e9c6f99d24b544a872854fb761f45a695b4
martincostello <martin@martincostello.com>
2022-09-03 11:42:57 +0100

    Remove NeedsUnsafeWithNoUnsafeCode
    Remove the NeedsUnsafeWithNoUnsafeCode repro as even without generating `unsafe` twice where not needed, it's needed for `[SkipLocalsInitAttribute]` which is there for performance.

--- NeedsUnsafeWithNoUnsafeCode/NativeMethods.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-internal static partial class NativeMethods
-{
-    [LibraryImport("kernel32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static partial bool FreeLibrary(IntPtr hModule);
-}
